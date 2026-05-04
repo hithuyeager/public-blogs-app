285d4d29800a1dce870aaaad50ef8c0f8cdc16b0
hithesh <hitheshrhithu41@gmail.com>
2026-05-04 04:17:53 +0000

    auth perfectly done

--- repositories/auth_repo.py
+++ repositories/auth_repo.py
@@ -15,7 +15,7 @@ async def username_exist(conn: asyncpg.Connection,username: str) -> bool:
 
 async def fetch_user_id(conn:asyncpg.Connection,username: str) -> str:
     user_id = await conn.fetchval("SELECT id FROM users_auth WHERE username = $1",username)
-    return user_id
+    return str(user_id)
 async def fetch_password(conn: asyncpg.Connection ,username: str) -> str:
     hash_password = await conn.fetchval("SELECT password FROM users_auth WHERE username = $1",username)
     return hash_password
@@ -30,11 +30,11 @@ async def fetch_refresh_token(conn: asyncpg.Connection,user_id: UUID) -> str:
     hashed_refresh_token = await conn.fetchval("SELECT hash_refresh_token FROM user_session WHERE user_id = $1",user_id)
     return hashed_refresh_token
 async def replace_refresh_token(conn: asyncpg.Connection,user_id: UUID,hashed_refresh_token: str) -> str:
-    status = await conn.fetchval("UPDATE TABLE user_session SET hash_refresh_token = $2 WHERE user_id = $1 RETURNING id",user_id,hashed_refresh_token)
+    status = await conn.fetchval("UPDATE user_session SET hash_refresh_token = $2 WHERE user_id = $1 RETURNING id",user_id,hashed_refresh_token)
     return str(status)
 async def make_user_active(conn: asyncpg.Connection,user_id: UUID) -> str:
-    status = await conn.fetchval("UPDATE TABLE user_session SET is_active = true WHERE user_id = $1 RETURNING id",user_id)
-    return str(id)
+    status = await conn.fetchval("UPDATE user_session SET is_active = true WHERE user_id = $1 RETURNING id",user_id)
+    return str(status)
 async def fetch_user_status(conn: asyncpg.Connection,user_id: UUID) -> bool:
     is_active = await conn.fetchval("SELECT is_active FROM user_session WHERE user_id = $1",user_id)
     return is_active
